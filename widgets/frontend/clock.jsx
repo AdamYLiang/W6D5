a8cbbf7597aa01181e54d2793f60419817fa21e9
AdamYLiang <ayl292@nyu.edu>
2019-01-11 13:54:15 -0500

    Hook React Up

---
+++ widgets/frontend/clock.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor (props) {
+    super(props);
+    
+  }
+
+  render() {
+    return ( 
+    <>
+      <h1>Hello</h1>
+    </>
+    )
+  }
+}
+
+export default Clock; 
