6f8fada44a27f1c11ce888b7b2ca9a855afd490a
AdamYLiang <ayl292@nyu.edu>
2019-01-11 14:22:36 -0500

    Working react clock

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -3,13 +3,35 @@ import React from 'react';
 class Clock extends React.Component {
   constructor (props) {
     super(props);
-    
+
+    this.state = { Date: new Date() }; 
+    this.tick = this.tick.bind(this);
+
+    this.ourSeconds = null; 
+  }
+
+  tick() {
+    this.setState( { Date: new Date() } );
+  }
+
+  componentDidMount() {
+    this.ourSeconds = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ourSeconds);
+    this.ourSeconds = 0;
   }
 
   render() {
+    // debugger
+    const hours = this.state.Date.getHours(); 
+    const minutes = this.state.Date.getMinutes(); 
+    const seconds = this.state.Date.getSeconds(); 
+
     return ( 
     <>
-      <h1>Hello</h1>
+      <h1>{hours}:{minutes}:{seconds}</h1>
     </>
     )
   }
