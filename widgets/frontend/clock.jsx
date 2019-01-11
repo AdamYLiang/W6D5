4dec6dd8957e4db27d136424df8143d288283161
AdamYLiang <ayl292@nyu.edu>
2019-01-11 15:06:35 -0500

    Style clock

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -20,18 +20,30 @@ class Clock extends React.Component {
 
   componentWillUnmount() {
     clearInterval(this.ourSeconds);
-    this.ourSeconds = 0;
+    // this.ourSeconds = 0;
   }
 
   render() {
     // debugger
-    const hours = this.state.Date.getHours(); 
-    const minutes = this.state.Date.getMinutes(); 
-    const seconds = this.state.Date.getSeconds(); 
+    // const hours = this.state.Date.getHours(); 
+    // const minutes = this.state.Date.getMinutes(); 
+    // const seconds = this.state.Date.getSeconds(); 
+    const date = this.state.Date.toDateString();
+    const time = this.state.Date.toLocaleTimeString();
 
     return ( 
     <>
-      <h1>{hours}:{minutes}:{seconds}</h1>
+      <section className="clockBox"> 
+        <div className="innerClockDiv">
+        <label>Time:</label>
+        <time className="time clock"> {time}</time>
+        </div>
+
+        <div className="innerClockDiv">
+        <label>Date:</label>
+        <time className="time date"> {date}</time>
+        </div>
+      </section>
     </>
     )
   }
