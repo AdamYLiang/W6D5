6a6e010891b5a473bd4de21d339e93dd4014be19
AdamYLiang <ayl292@nyu.edu>
2019-01-11 17:25:17 -0500

    CSS Styled

--- widgets/widgets.jsx
+++ widgets/widgets.jsx
@@ -4,8 +4,22 @@ import ReactDOM from 'react-dom';
 import Clock from './frontend/clock'; 
 import Tabs from './frontend/tabs';
 
+
+function Root() {
+  return (
+    <div id="main">
+      <Clock />
+      <Tabs />
+    </div>
+  );
+}
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById("root");
-    const tabbings = React.createElement(Tabs);
-    ReactDOM.render(tabbings, root);
+    // const tabbings = React.createElement(Tabs);
+    ReactDOM.render(<Root />, root);
 });
